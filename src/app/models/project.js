bd2c53ae51c34710f8864c150bd2330b6f28459d
oskojess <oskojess@gmail.com>
2019-04-28 11:48:05 -0300

    add useradmin and  project status

--- src/app/models/project.js
+++ src/app/models/project.js
@@ -1,66 +1,77 @@
-const mongoose = require('../../database');
-const bcrypt = require('bcryptjs');
+    const mongoose = require('../../database');
+    
+    const ProjectSchema = new mongoose.Schema({
+        title: {
+            type: String,
+            required: true,
+        },
 
-const ProjectSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
+        description: {
+            type: String,
+            required: true,
+        },
 
-    description: {
-        type: String,
-        required: true,
-    },
+        category: {
+            type: String, 
+            required: false,
+        }, 
 
-    category: {
-        type: String, 
-        required: false,
-    }, 
+        objective:{
+            type: String, 
+            required: false,
+        },
 
-    objective:{
-        type: String, 
-        required: false,
-    },
+        project_format:{
+            type: String, 
+            required: false,
+        },
 
-    project_format:{
-        type: String, 
-        required: false,
-    },
+        list_needs: {
+            type: String, 
+            required: false,
+        },
 
-    list_needs: {
-        type: String, 
-        required: false,
-    },
+        local: {
+            type: String, 
+            required: false,
+        }, 
 
-    local: {
-        type: String, 
-        required: false,
-    }, 
+        tags:{
+            type: String, 
+            required: false,
+        },
 
-    tags:{
-        type: String, 
-        required: false,
-    },
+        value:{
+            type: Number, 
+            required: false,
+        },
 
-    value:{
-        type: Number, 
-        required: false,
-    },
+        status: 
+        {
+            pendent: {
+                type: Boolean,
+                default: 'true',
+            },
+            approved: {
+                type: Boolean, 
+                    default: 'false',
+            },
+        },
+
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User', 
+            require: true,
+        }, 
 
 
-    createAt: {
-        type: Date, 
-        default: Date.now,
-    },
+        createAt: {
+            type: Date, 
+            default: Date.now,
+        },
 
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User', 
-        require: true,
-    }
+    })
 
-})
+    const Project = mongoose.model('Project', ProjectSchema);
 
-const Project = mongoose.model('Project', ProjectSchema);
-
-module.exports = Project;
+    module.exports = Project;
