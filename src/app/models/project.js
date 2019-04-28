4cbf7b41096a985da12a1c78d414373e4ad14e02
oskojess <oskojess@gmail.com>
2019-04-27 21:44:28 -0300

    add types in project model

--- src/app/models/project.js
+++ src/app/models/project.js
@@ -6,21 +6,59 @@ const ProjectSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+
     description: {
         type: String,
         required: true,
+    },
+
+    category: {
+        type: String, 
+        required: false,
+    }, 
+
+    objective:{
+        type: String, 
+        required: false,
+    },
+
+    project_format:{
+        type: String, 
+        required: false,
+    },
+
+    list_needs: {
+        type: String, 
+        required: false,
+    },
+
+    local: {
+        type: String, 
+        required: false,
+    }, 
+
+    tags:{
+        type: String, 
+        required: false,
+    },
+
+    value:{
+        type: Number, 
+        required: false,
+    },
+
+
+    createAt: {
+        type: Date, 
+        default: Date.now,
     },
 
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User', 
         require: true,
-    },
-
-    createAt: {
-        type: Date, 
-        default: Date.now,
     }
+
 })
 
 const Project = mongoose.model('Project', ProjectSchema);
